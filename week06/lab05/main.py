7efeb40075ed8f236eca553f8369b53ec9a9bc2a
vivobook2024 <jyang@stut.edu.tw>
2024-03-27 22:59:33 +0800

    home

---
+++ week06/lab05/main.py
@@ -0,0 +1,7 @@
+def remove_duplicates(numbers):
+    # replace pass with your code
+    pass
+
+if __name__ == "__main__":
+    numbers = list(map(int, input().split()))
+    print(remove_duplicates(numbers))
